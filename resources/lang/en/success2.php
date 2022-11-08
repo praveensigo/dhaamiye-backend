de04147c58ffcd8dffc80ded1161fc78f262c534
AiswaryaCD <112742671+AiswaryaCD@users.noreply.github.com>
2022-11-08 16:56:07 +0530

    Register Controller Added (#4)

---
+++ resources/lang/en/success2.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    //Driver
+    'create_driver' => 'Driver Added successfully',
+    'driver_update' => 'Driver updated successfully',
+    'block_driver'=>'Driver published successfully',
+
+    //TRUCK
+    'register_success' => 'Truck added successfully',
+    'truck_update' => 'Truck updated successfully',
+    'block_truck'=>'Truck published successfully',
+
+
+    'create_coupons' =>  'Coupon created successfully',
+
+
+];
