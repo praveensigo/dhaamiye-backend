1ac09347898325b2d10040234f18d693ae8a69c7
shanasigo <97424683+shanasigo@users.noreply.github.com>
2022-11-09 13:16:53 +0530

    migrations updated (#6)

--- database/migrations/2022_10_17_054536_create_trucks_table.php
+++ database/migrations/2022_10_17_054536_create_trucks_table.php
@@ -30,13 +30,17 @@ return new class extends Migration
             $table->string('truck_certificate_url')->nullable();
             $table->date('truck_certificate_expiry')->nullable();
             $table->integer('added_by')->nullable()->comment('1:Admin,2:Sub admin,4:Self');
-            $table->integer('added_user')->references('user_id')->on('users')->nullable();
+            $table->integer('added_user')->references('id')->on('users')->nullable();
             $table->integer('updated_by')->nullable()->comment('1:Admin,2:Sub admin,4:Self');
-            $table->foreignId('updated_user')->references('user_id')->on('users')->nullable();
+            $table->foreignId('updated_user')->references('id')->on('users')->nullable();
             $table->integer('approval_by')->nullable()->comment('1:Admin,2:Sub admin,5:Fuel station');
+<<<<<<< HEAD:database/migrations/2022_10_17_054536_create_trucks_table.php
+            $table->foreignId('approval_user')->references('id')->on('users')->nullable();            
+            $table->integer('reg_status')->comment('0:Pending ,1:Accepted, 2:Rejected');
+=======
             $table->foreignId('approval_user')->references('user_id')->on('users')->nullable();            
+>>>>>>> main:database/migrations/2022_10_19_074536_create_trucks_table.php
             $table->integer('status')->default(1)->comment('1:Active, 2:Blocked');
-            $table->integer('reg_status')->default(0)->comment('0:Pending,1:Accepted,2:Rejected');
             $table->timestamps();
         });
     }
