f7dec4fde3a7d8afd8f6142fdcdc52bc790b1edb
Pressy Manoj <pressy.sigosoft@gmail.com>
2022-12-03 10:59:15 +0530

    notifications

--- routes/customer.php
+++ routes/customer.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\android\customer\OrderController;
 use App\Http\Controllers\android\customer\ReviewController;
 use App\Http\Controllers\android\customer\FavoriteController;
 use App\Http\Controllers\android\customer\PromotionController;
+use App\Http\Controllers\android\customer\NotificationsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -53,4 +54,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('favourites/add-remove', [FavoriteController::class, 'addRemove']);
     Route::get('favourites', [FavoriteController::class, 'index']);
     Route::get('promotions', [PromotionController::class, 'index']);
+    Route::get('notifications', [NotificationsController::class, 'index']);
 });
