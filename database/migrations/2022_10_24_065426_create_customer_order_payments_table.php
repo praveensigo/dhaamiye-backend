cde304d61d2459c89a32fabaa0e599ff6257f7a4
Pressy Manoj <pressy.sigosoft@gmail.com>
2022-12-03 15:42:42 +0530

    updates

--- database/migrations/2022_10_24_065426_create_customer_order_payments_table.php
+++ database/migrations/2022_10_24_065426_create_customer_order_payments_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('order_id')->references('id')->on('customer_orders');
             $table->foreignId('customer_id')->references('id')->on('customers');
-            $table->foreignId('driver_id')->references('id')->on('drivers');
+            $table->foreignId('driver_id')->references('id')->on('drivers')->nullable();
             $table->integer('payment_type')->comment('1:Mobile, 2:Cash')->nullable();
             $table->string('payment_id')->nullable();
             $table->decimal('total_amount', 12, 2);
