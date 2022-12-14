e99fb9f6fd04ecf0fff8141f29036e6787f3066b
Pressy Manoj <pressy.sigosoft@gmail.com>
2022-12-14 13:10:38 +0530

    driver app profile, home

---
+++ resources/lang/en/driver_error.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	'profile_image_max_en' => 'Maximum file size: 1Mb',
+    'profile_image_max_so' => 'Maximum file size: 1Mb',
+
+    'accept_order_en' => 'Failed to accept order',
+    'accept_order_so' => 'Failed to accept order',
+];
