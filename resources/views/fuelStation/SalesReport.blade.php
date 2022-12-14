57c1f60b9d8cd5e77ab72e9028a7e34c03214205
AiswaryaCD <aiswarya.sigosoft@gmail.com>
2022-12-14 15:07:48 +0530

    Reports Updated

--- resources/views/fuelStation/SalesReport.blade.php
+++ resources/views/fuelStation/SalesReport.blade.php
@@ -183,7 +183,7 @@
 						<tr>
 							<td class="text-center">{{ $order->sl_no }}</td>
 							<td>{{ $order->order_id }}</td>
-							<td>{{ $order->date }}</td>
+							<td>{{ date('d-m-Y',strtotime($order->date)) }}</td>
 							<td>{{ $order->fuel_quantity_price }}</td>
 							<td>{{ $order->delivery_charge }}</td>
 							<td>{{ $order->tax }}</td>
