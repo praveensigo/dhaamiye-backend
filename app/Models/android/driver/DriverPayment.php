bb6134215773a420414f83c776efefcb368e4ce5
pressy-sigosoft <88468240+pressy-sigosoft@users.noreply.github.com>
2023-01-02 17:43:28 +0530

    Pressy (#134)
    * truck table migration update
    
    * customer login
    
    * updates
    
    * forgot password, reset password, register check
    
    * fuel type seeder
    
    * home, search
    
    * fuels, check stock
    
    * apply coupon, request fuel
    
    * order APIs
    
    * favourites
    
    * promotions
    
    * promotions
    
    * bug
    
    * updates
    
    * updates
    
    * updates
    
    * get coutries, login with otp
    
    * notifications
    
    * notifications
    
    * add notification in order
    
    * country code id
    
    * cms
    
    * updates
    
    * updates
    
    * add enquiry
    
    * distance
    
    * updates
    
    * updates
    
    * profile edit - check mobile
    
    * my orders update
    
    * my orders update
    
    * meter reading images
    
    * payment type added in order details
    
    * contact, about, terms, privacy, settings seeder and updates
    
    * update
    
    * request and book now array change
    
    * book now update
    
    * min order amount checking in apply coupon
    
    * update
    
    * append distance
    
    * append distance fuel types
    
    * append distance request fuel, book now
    
    * update
    
    * driver app profile, home
    
    * update
    
    * update
    
    * updates
    
    * scheduled status update
    
    * order details update
    
    * order details update
    
    * post pin and other updates
    
    * post pin and other updates
    
    * rate and review
    
    * view documents, upload documents
    
    * fuel station added in driver profile
    
    * driver inbox, upload meter images
    
    * earnings
    
    * earnings updates
    
    * updates
    
    * review in order details
    
    * online/offline
    
    * converted attributes

--- app/Models/android/driver/DriverPayment.php
+++ app/Models/android/driver/DriverPayment.php
@@ -9,8 +9,32 @@ class DriverPayment extends Model
 {
     use HasFactory;
 
+    protected $appends = [
+        'converted_created_at',
+        'converted_payment_type',
+    ];
+
     public function order() {
-        return $this->hasOne(CustomerOrder::class, 'order_id', 'id');
+        return $this->hasOne(CustomerOrder::class, 'id', 'order_id');
+    }
+
+    /**
+    *** Accessors
+    **/
+    public function getConvertedCreatedAtAttribute()
+    {
+        return date('d M Y, h:i a', strtotime($this->created_at));
+    }
+    public function getConvertedPaymentTypeAttribute() {
+        if($this->payment_type == 1) {
+            return 'Mobile Payment';
+
+        } else if($this->payment_type == 2) {
+            return 'Cash Payment';
+
+        } else {
+            return '';
+        }
     }
 
 }
