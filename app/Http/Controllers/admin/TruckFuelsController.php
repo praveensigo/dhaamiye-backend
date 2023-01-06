501de78a9124e8ab4a63133d606bf2064eedd81f
AiswaryaCD <aiswarya.sigosoft@gmail.com>
2023-01-06 17:26:24 +0530

    Truck Stock checking updated

--- app/Http/Controllers/admin/TruckFuelsController.php
+++ app/Http/Controllers/admin/TruckFuelsController.php
@@ -169,6 +169,12 @@ public function updateStock(Request $request)
                         ->first();
             if($fields['stock'] <= $fuel_station_stock->stock)
             {
+            $truck_stock_details2 = DB::table('truck_fuels')->select('truck_fuels.*')->where('id',$fields['id'])->first();
+            $current_stock = $truck_stock_details2->stock;
+            $capacity = $truck_stock_details2->capacity;
+            $check = $current_stock + $fields['stock'];
+            if($check < $capacity)
+            {
             $truck_fuel = TruckFuel::find($fields['id']);
             $truck_fuel->stock = $truck_fuel->stock + $fields['stock'];
             $truck_fuel->updated_at = date('Y-m-d H:i:s');
@@ -205,6 +211,10 @@ public function updateStock(Request $request)
                 $res = Response::send(true, [], __('success.update_stock'), 200);
             } }}else {
                 $res = Response::send(false, [], __('error.update_stock'), 400);
+
+            }}
+            else {
+                $res = Response::send(false, [], 'Please make sure that the stock is less than or equal to the capacity', 400);
 
             }
         }
