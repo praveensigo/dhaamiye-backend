01f43e5b3a57f971a159fcc81932a41f7fac8d94
pressy-sigosoft <88468240+pressy-sigosoft@users.noreply.github.com>
2023-01-09 11:45:04 +0530

    Pressy (#154)
    * truck table migration update
    
    * customer login
    
    * updates
    
    * forgot password, reset password, register check
    
    * fuel type seeder
    
    * home, search
    
    * fuels, check stock
    
    * apply coupon, request fuel
    
    * order APIs
    
    * favourites
    
    * promotions
    
    * promotions
    
    * bug
    
    * updates
    
    * updates
    
    * updates
    
    * get coutries, login with otp
    
    * notifications
    
    * notifications
    
    * add notification in order
    
    * country code id
    
    * cms
    
    * updates
    
    * updates
    
    * add enquiry
    
    * distance
    
    * updates
    
    * updates
    
    * profile edit - check mobile
    
    * my orders update
    
    * my orders update
    
    * meter reading images
    
    * payment type added in order details
    
    * contact, about, terms, privacy, settings seeder and updates
    
    * update
    
    * request and book now array change
    
    * book now update
    
    * min order amount checking in apply coupon
    
    * update
    
    * append distance
    
    * append distance fuel types
    
    * append distance request fuel, book now
    
    * update
    
    * driver app profile, home
    
    * update
    
    * update
    
    * updates
    
    * scheduled status update
    
    * order details update
    
    * order details update
    
    * post pin and other updates
    
    * post pin and other updates
    
    * rate and review
    
    * view documents, upload documents
    
    * fuel station added in driver profile
    
    * driver inbox, upload meter images
    
    * earnings
    
    * earnings updates
    
    * updates
    
    * review in order details
    
    * online/offline
    
    * converted attributes
    
    * review update, contact us, cms, get fuel stations
    
    * route update
    
    * track driver, register check API update
    
    * get fuel stations update
    
    * distance correction
    
    * truck stock, migrations updates, seeder
    
    * update
    
    * map api distance bug fixed
    
    * map api distance bug fixed
    
    * bug fixed
    
    * fuel station select issues fixed
    
    * fuel station select issues fixed
    
    * update
    
    * ambigous bug fixed

--- app/Http/Controllers/android/customer/OrderController.php
+++ app/Http/Controllers/android/customer/OrderController.php
@@ -265,7 +265,9 @@ class OrderController extends Controller
                     },
                 ])
 
-                ->where('id', $request->fuel_station_id)
+
+                ->where('fuel_stations.id', $request->fuel_station_id)
+
 
                 ->first();
 
@@ -420,7 +422,9 @@ class OrderController extends Controller
                 ->active()
                 ->where('role_id', 5)      
 
-                ->where('id', $request->fuel_station_id)          
+
+                ->where('fuel_stations.id', $request->fuel_station_id)          
+
 
                 ->first();
 
